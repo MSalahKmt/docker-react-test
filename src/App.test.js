606cd31153db86c5024d1b35984dad0e99e8d71d
msalahkmt <mohamedsalahkamate@gmail.com>
2023-01-19 11:43:48 -0500

    update test file

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hi there/i);
+  const linkElement = screen.getByText(/Quick test change in the feature1 branch/i);
   expect(linkElement).toBeInTheDocument(); 
 });
